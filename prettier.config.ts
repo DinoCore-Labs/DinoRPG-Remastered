da7cc2cac1925fe8d64fe3a70e0c1c3635a285aa
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2025-11-16 14:48:21 +0100

    chore(client): initial Vue 3 + Vite setup with TypeScript, ESLint and Prettier

---
+++ prettier.config.ts
@@ -0,0 +1,14 @@
+import { type Config } from 'prettier';
+
+const config: Config = {
+	plugins: [],
+	printWidth: 120,
+	semi: true,
+	singleQuote: true,
+	useTabs: true,
+	trailingComma: 'none',
+	bracketSpacing: true,
+	arrowParens: 'avoid'
+};
+
+export default config;
