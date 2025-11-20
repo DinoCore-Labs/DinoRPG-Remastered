a9a59010f6298204b75a5217d0098d40fef6f22c
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2025-11-20 22:53:35 +0100

    feat: mainpage & leftpanel

---
+++ app/client/src/pages/MainPage.vue
@@ -0,0 +1,83 @@
+<template>
+	<div class="mainpage">
+		<div class="mainpage-header">
+			<a @click="goToMainPage()" class="linkHome"></a>
+			<LeftPanel />
+			<div class="mainpage-center">
+				<RouterView />
+			</div>
+		</div>
+		<div class="mainpage-footer"></div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import LeftPanel from '../components/common/LeftPanel.vue';
+
+export default defineComponent({
+	name: 'MainPage',
+	components: { LeftPanel },
+	methods: {
+		async goToMainPage() {
+			this.$router.push({
+				name: 'MainPage'
+			});
+		}
+	}
+});
+</script>
+
+<style lang="scss" scoped>
+.mainpage {
+	background-image: url('../assets/background/bg_ciel.webp');
+	background-repeat: repeat-x;
+	&-header {
+		min-height: 100vh;
+		background:
+			url('../assets/background/full_bg.webp') no-repeat,
+			url('../assets/background/full_core_bg.webp') repeat-y;
+		background-position-x: calc(50% + 247px);
+		background-position-y: top;
+		padding-bottom: 50px;
+		.linkHome {
+			grid-area: top;
+			cursor: pointer;
+			height: 11rem;
+			width: 100%;
+			max-width: 540px;
+		}
+	}
+	&-center {
+		grid-area: center;
+		width: 100%;
+		max-width: 640px;
+		display: flex;
+		flex-direction: column;
+		z-index: 1;
+	}
+	&-footer {
+		background-image: url('../assets/background/full_footer.webp');
+		background-color: white;
+		background-repeat: no-repeat;
+		background-position-x: calc(50% + 248px);
+		min-height: 128px;
+	}
+}
+@media (min-width: 875px) {
+	.mainpage-header {
+		display: grid;
+		grid-template-areas: 'left top top' 'left center center';
+		padding-top: 15px;
+		grid-template-columns: 1fr 540px 1fr;
+		grid-template-rows: 110px 1fr;
+	}
+}
+@media (max-width: 875px) {
+	.mainpage-header {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+	}
+}
+</style>
--- app/client/src/router/index.ts
+++ app/client/src/router/index.ts
@@ -2,12 +2,18 @@ import type { RouteRecord } from '@dinorpg/core/models/router/Router';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import HomePage from '../pages/HomePage.vue';
+import MainPage from '../pages/MainPage.vue';
 
 const routes: RouteRecord[] = [
 	{
 		path: '/',
 		name: 'HomePage',
 		component: HomePage
+	},
+	{
+		path: '/main',
+		name: 'MainPage',
+		component: MainPage
 	}
 ];
 
