ffde90ba10b695e22e3d3711684a815f5071ebc7
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2025-11-20 13:22:48 +0100

    chore: setup VueDevTools

--- app/client/vite.config.ts
+++ app/client/vite.config.ts
@@ -1,9 +1,10 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), VueDevTools()],
 	server: {
 		port: 8000,
 		host: '0.0.0.0',
