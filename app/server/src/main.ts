e9bb590f85b75e5b95039a51a7c88b7e7a41b851
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2025-11-23 00:16:34 +0100

    feat: user routes, user schema and init prisma

--- app/server/src/main.ts
+++ app/server/src/main.ts
@@ -1,19 +1,40 @@
 import Fastify from 'fastify';
 
-const fastify = Fastify({
+import { userRoutes } from './routes/user.routes.js';
+import { userSchemas } from './schema/user.schema.js';
+
+const app = Fastify({
 	logger: true
 });
 
-fastify.get('/', async () => {
+// register schemas
+for (let schema of [...userSchemas]) {
+	app.addSchema(schema);
+}
+
+// routes
+app.register(userRoutes, { prefix: 'api/users' });
+
+app.get('/', async () => {
 	return { message: 'Hello World!' };
 });
+app.get('/healthcheck', (req, res) => {
+	res.send({ message: 'Success' });
+});
 
-const start = async () => {
-	try {
-		await fastify.listen({ port: 3000 });
-	} catch (err) {
-		fastify.log.error(err);
-		process.exit(1);
-	}
-};
-start();
+// graceful shutdown
+const listeners = ['SIGINT', 'SIGTERM'];
+listeners.forEach(signal => {
+	process.on(signal, async () => {
+		await app.close();
+		process.exit(0);
+	});
+});
+
+async function main() {
+	await app.listen({
+		port: 8081,
+		host: '0.0.0.0'
+	});
+}
+main();
---
+++ app/server/src/routes/user.routes.ts
@@ -0,0 +1,39 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+
+import { $ref } from '../schema/user.schema.js';
+
+export async function userRoutes(app: FastifyInstance) {
+	app.get('/', (req: FastifyRequest, reply: FastifyReply) => {
+		reply.send({ message: '/ route hit' });
+	});
+
+	app.post(
+		'/register',
+		{
+			schema: {
+				body: $ref('createUserSchema'),
+				response: {
+					201: $ref('createUserResponseSchema')
+				}
+			}
+		},
+		() => {}
+	);
+
+	app.post(
+		'/login',
+		{
+			schema: {
+				body: $ref('loginSchema'),
+				response: {
+					201: $ref('loginResponseSchema')
+				}
+			}
+		},
+		() => {}
+	);
+
+	app.delete('/logout', () => {});
+
+	app.log.info('user routes registered');
+}
