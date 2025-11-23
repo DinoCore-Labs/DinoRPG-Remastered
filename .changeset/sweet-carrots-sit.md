01bedea4d26cfa9d754aacfc9051bd7fc68d5a05
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2025-11-23 01:02:05 +0100

    feat: user controller

---
+++ .changeset/sweet-carrots-sit.md
@@ -0,0 +1,5 @@
+---
+'@dinorpg/client': patch
+---
+
+[c6ba44a](https://github.com/DinoCore-Labs/DinoRPG-Remastered/commit/c6ba44a): feat authMenu
---
+++ app/server/src/controller/user.controller.ts
@@ -0,0 +1,41 @@
+import bcrypt from 'bcrypt';
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+import { prisma } from '../prisma.js';
+import { CreateUserInput } from '../schema/user.schema.js';
+
+const SALT_ROUNDS = 10;
+
+export async function createUser(
+	req: FastifyRequest<{
+		Body: CreateUserInput;
+	}>,
+	reply: FastifyReply
+) {
+	const { password, name } = req.body;
+
+	const user = await prisma.user.findUnique({
+		where: {
+			name: name
+		}
+	});
+	if (user) {
+		return reply.code(401).send({
+			message: 'User already exists with this name'
+		});
+	}
+
+	try {
+		const hash = await bcrypt.hash(password, SALT_ROUNDS);
+		const user = await prisma.user.create({
+			data: {
+				password: hash,
+				name
+			}
+		});
+
+		return reply.code(201).send(user);
+	} catch (e) {
+		return reply.code(500).send(e);
+	}
+}
