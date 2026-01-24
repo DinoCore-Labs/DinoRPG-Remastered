676aa2e3b38501e1b045f31665a92a0ffe0652e3
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-01-24 15:10:26 +0100

    refactor: use dztable component for ranking pages

--- app/client/src/components/rankings/PlayerRanking.vue
+++ app/client/src/components/rankings/PlayerRanking.vue
@@ -2,54 +2,52 @@
 	<DZDisclaimer content="ranking.disclaimer.classic" help v-if="sort === 'classic'" />
 	<DZDisclaimer content="ranking.disclaimer.average" help v-if="sort === 'average'" />
 	<div class="wrapper">
-		<table>
-			<tbody>
-				<tr>
-					<th class="pos">{{ $t('ranking.th.pos') }}</th>
-					<th class="player">{{ $t('ranking.th.player') }}</th>
-					<th class="dinoz">Dinoz</th>
-					<th class="points">{{ $t('ranking.th.points') }}</th>
-					<th class="points">{{ $t('ranking.th.average') }}</th>
-				</tr>
-				<tr class="select" @click="changePage(-1)" v-if="page > 1">
-					<td class="pos" colspan="5" style="text-align: center">
-						{{ $t('ranking.page.previous') }}
-					</td>
-				</tr>
-				<tr
-					v-for="(ranking, index) in rankings"
-					:key="ranking.user.id"
-					class="select"
-					:class="{
-						even: (index + 1) % 2 === 0
-					}"
-					@click="selectedUser = ranking.user.id"
-				>
-					<td class="pos">
-						{{ (page - 1) * 20 + (index + 1) }}
-					</td>
-					<td class="other">
-						<DZUser :user="ranking.user" :me="ranking.user.id === me" :friend="false" />
-					</td>
-					<td class="other">
-						{{ ranking.dinozCount }}
-					</td>
-					<td class="other">
-						{{ ranking.points }}
-					</td>
-					<td class="other">
-						{{ ranking.average }}
-					</td>
-				</tr>
-				<UserCard v-if="seeUser && selectedUser" :userId="selectedUser" />
-			</tbody>
+		<DZTable>
+			<tr>
+				<th class="thPos">{{ $t('ranking.th.pos') }}</th>
+				<th class="thPlayer">{{ $t('ranking.th.player') }}</th>
+				<th class="thDinoz">Dinoz</th>
+				<th class="thPoints">{{ $t('ranking.th.points') }}</th>
+				<th class="thPoints">{{ $t('ranking.th.average') }}</th>
+			</tr>
+			<tr class="select" @click="changePage(-1)" v-if="page > 1">
+				<td class="pos" colspan="5" style="text-align: center">
+					{{ $t('ranking.page.previous') }}
+				</td>
+			</tr>
+			<tr
+				v-for="(ranking, index) in rankings"
+				:key="ranking.user.id"
+				class="select"
+				:class="{
+					even: (index + 1) % 2 === 0
+				}"
+				@click="selectedUser = ranking.user.id"
+			>
+				<td class="tdPos">
+					{{ (page - 1) * 20 + (index + 1) }}
+				</td>
+				<td class="tdOther">
+					<DZUser :user="ranking.user" :me="ranking.user.id === me" :friend="false" />
+				</td>
+				<td class="tdOther">
+					{{ ranking.dinozCount }}
+				</td>
+				<td class="tdOther">
+					{{ ranking.points }}
+				</td>
+				<td class="tdOther">
+					{{ ranking.average }}
+				</td>
+			</tr>
+			<UserCard v-if="seeUser && selectedUser" :userId="selectedUser" />
 			<!-- TODO : A CORRIGER TR/TD NE PEUT PAS ETRE EN DEHORS DE TBODY -->
 			<tr class="select" @click="changePage(1)" :class="{ hidden: rankings.length < 20 }">
 				<td class="pos" colspan="5" style="text-align: center">
 					{{ $t('ranking.page.next') }}
 				</td>
 			</tr>
-		</table>
+		</DZTable>
 	</div>
 	<DZSearch background entityType="user" placeHolder="ranking.placeholder.searchUser" @entity="goToAccount" />
 </template>
@@ -58,13 +56,13 @@
 import type { RankingGetResponses } from '@dinorpg/core/models/ranking/rankingGetResponse.js';
 
 import { defineComponent, type PropType } from 'vue';
-//import eventBus from '../../events/index.js';
 import { RankingService } from '../../services';
 import { userStore } from '../../store/userStore';
 import { errorHandler } from '../../utils/errorHandler';
 import DZUser from '../utils/DZUser.vue';
 import DZDisclaimer from '../utils/DZDisclaimer.vue';
 import DZSearch from '../utils/DZSearch.vue';
+import DZTable from '../utils/DZTable.vue';
 import UserCard from '../modal/UserCard.vue';
 
 export default defineComponent({
@@ -72,6 +70,7 @@ export default defineComponent({
 	components: {
 		DZSearch,
 		DZDisclaimer,
+		DZTable,
 		DZUser,
 		UserCard
 	},
@@ -106,10 +105,8 @@ export default defineComponent({
 			this.$router.push({ name: 'MyAccount', params: { id: user.id } });
 		},
 		async getRanking(): Promise<void> {
-			//EventBus.emit('isLoading', true);
 			try {
 				this.rankings = await RankingService.getRanking(this.sort, this.page);
-				//EventBus.emit('isLoading', false);
 			} catch (err) {
 				errorHandler.handle(err, this.$toast);
 				return;
@@ -141,88 +138,34 @@ export default defineComponent({
 .wrapper {
 	margin: 5px;
 	position: relative;
-	table {
-		width: 100%;
-		margin-top: 10px;
-		margin-bottom: 5px;
-		margin-bottom: 10px;
-		border: 2px solid #f3d6b1;
-		background-color: #ecbd84;
-		border-collapse: separate;
-		border-spacing: 1px;
-		tr {
-			display: table-row;
-			th {
-				font-size: 8pt;
-				letter-spacing: 0pt;
-				text-shadow: 1px 1px 0px #356847;
-				padding-left: 4px;
-				padding-right: 4px;
-				padding-bottom: 8px;
-				height: 41px;
-				vertical-align: bottom;
-				color: #fffdba;
-				text-transform: uppercase;
-				font-weight: bold;
-				letter-spacing: 1pt;
-				text-align: left;
-				white-space: nowrap;
-				border: 1px solid #356847;
-				background-color: #c64e36;
-				background-image: url('../../assets/background/table_header.webp');
-				background-position: left bottom;
-				max-width: 222px;
-				&.pos {
-					width: 5em;
-				}
-				&.player {
-					max-width: 150px;
-				}
-				&.dinoz {
-					max-width: 15px;
-				}
-				&.points {
-					max-width: 15px;
-				}
-			}
-			td {
-				font-size: 9pt;
-				padding-right: 5px;
-				padding-top: 1px;
-				padding-bottom: 1px;
-				color: #710;
-				background-color: #f3ca92;
-				border: 1px solid #c88f44;
-				cursor: pointer;
-				&.pos {
-					background-image: url('../../assets/background/table_cell.webp');
-					background-position: 0px 0px;
-					padding-left: 1.2em;
-				}
-				&.other {
-					padding-left: 1em;
-					background-image: url('../../assets/background/table_cell.webp');
-					background-position: -10px 0px;
-					max-width: 4px;
-				}
-			}
-			&.even {
-				td.pos {
-					background-image: url('../../assets/background/table_cell_even.webp');
-					background-position: 0px 0px;
-				}
-				td.other {
-					background-image: url('../../assets/background/table_cell_even.webp');
-					background-position: -10px 0px;
-				}
-			}
-			&.select:hover {
-				td {
-					color: white;
-					border-color: #9a4029;
-				}
-			}
-		}
+}
+.thPos {
+	width: 5em;
+}
+.thPlayer {
+	max-width: 150px;
+}
+.thDinoz {
+	max-width: 15px;
+}
+.thPoints {
+	max-width: 15px;
+}
+.tdPos {
+	background-image: url('../../assets/background/table_cell.webp');
+	background-position: 0px 0px;
+	padding-left: 1.2em;
+}
+.tdOther {
+	padding-left: 1em;
+	background-image: url('../../assets/background/table_cell.webp');
+	background-position: -10px 0px;
+	max-width: 4px;
+}
+.select:hover {
+	td {
+		color: white;
+		border-color: #9a4029;
 	}
 }
 .hidden {
