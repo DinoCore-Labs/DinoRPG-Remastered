f8f8cb665ad31cc6136e9d6608fbcabf6729f345
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-02-18 21:00:10 +0100

    feat(backend): useItem function

--- app/server/src/Inventory/Routes/inventory.routes.ts
+++ app/server/src/Inventory/Routes/inventory.routes.ts
@@ -3,9 +3,11 @@ import { FastifyInstance } from 'fastify';
 import { equipItem } from '../Service/equipItem.service.js';
 import { getAllIngredientsData } from '../Service/getAllIngredientsData.service.js';
 import { getAllItemsData } from '../Service/getAllItemsData.service.js';
+import { useItemHandler } from '../Service/useItem.service.js';
 
 export async function inventoryRoutes(app: FastifyInstance) {
 	app.get('/all/items', { preHandler: [app.authenticate], handler: getAllItemsData });
 	app.get('/all/ingredients', { preHandler: [app.authenticate], handler: getAllIngredientsData });
+	app.get('/:dinozId/:itemId', { preHandler: app.authenticate }, useItemHandler);
 	app.put('/equip/:dinozId', { preHandler: app.authenticate }, equipItem);
 }
