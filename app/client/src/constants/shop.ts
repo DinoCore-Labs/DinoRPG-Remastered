6bc1fd70846f806c85f885763c2c61d97fc2ce29
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-02-26 22:27:53 +0100

    feat: shop items page

---
+++ app/client/src/constants/shop.ts
@@ -0,0 +1,34 @@
+export const shopNameList: Array<string> = [
+	'unknown',
+	'flying',
+	'forge',
+	'magic',
+	'cursed', // picture missing
+	'fruity',
+	'razad',
+	'souk',
+	'neerhel',
+	'barbarian',
+	'secret',
+	'elit', // original game has no picture
+	'chens',
+	'merchant',
+	'merchant',
+	'merchant',
+	'merchant',
+	'merchant',
+	'merchant',
+	'merchant',
+	'filou'
+];
+
+export const itinerantShopNameList: Array<string> = [
+	'unknown',
+	'merchant_monday',
+	'merchant_tuesday',
+	'merchant_wednesday',
+	'merchant_thursday',
+	'merchant_friday',
+	'merchant_saturday',
+	'merchant_sunday'
+];
