c514da7ad249bd78c1b0e11137e179c4c97eb850
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-02-28 15:22:52 +0100

    fix(core): correct misaligned ingredientId values in ingredientList

--- packages/core/src/models/ingredients/ingredientList.ts
+++ packages/core/src/models/ingredients/ingredientList.ts
@@ -115,169 +115,169 @@ export const ingredientList: Readonly<Record<Ingredient, IngredientFiche>> = {
 	},
 	[Ingredient.GRIFFES_ACEREES]: {
 		name: 'griffes_acerees',
-		ingredientId: 8,
+		ingredientId: 7,
 		price: 750,
 		maxQuantity: 20
 	},
 	[Ingredient.CORNE_EN_CHOCOLAT]: {
 		name: 'corne_en_chocolat',
-		ingredientId: 9,
+		ingredientId: 8,
 		price: 1000,
 		maxQuantity: 5
 	},
 	[Ingredient.OEIL_VISQUEUX]: {
 		name: 'oeil_visqueux',
-		ingredientId: 10,
+		ingredientId: 9,
 		price: 1300,
 		maxQuantity: 5
 	},
 	[Ingredient.LANGUE_MONSTRUEUSE]: {
 		name: 'langue_monstrueuse',
-		ingredientId: 11,
+		ingredientId: 10,
 		price: 10000,
 		maxQuantity: 5
 	},
 	[Ingredient.DENT_DE_DOROGON]: {
 		name: 'dent_de_dorogon',
-		ingredientId: 33,
+		ingredientId: 11,
 		price: 12000,
 		maxQuantity: 5
 	},
 	[Ingredient.ROCHE_RADIO_ACTIVE]: {
 		name: 'roche_radio_active',
-		ingredientId: 7,
+		ingredientId: 12,
 		price: 500,
 		maxQuantity: 20
 	},
 	[Ingredient.ENERGIE_FOUDRE]: {
 		name: 'energie_foudre',
-		ingredientId: 12,
+		ingredientId: 13,
 		price: 300,
 		maxQuantity: 50
 	},
 	[Ingredient.ENERGIE_AIR]: {
 		name: 'energie_air',
-		ingredientId: 13,
+		ingredientId: 14,
 		price: 1000,
 		maxQuantity: 20
 	},
 	[Ingredient.ENERGIE_EAU]: {
 		name: 'energie_eau',
-		ingredientId: 14,
+		ingredientId: 15,
 		price: 4000,
 		maxQuantity: 5
 	},
 	[Ingredient.ENERGIE_FEU]: {
 		name: 'energie_feu',
-		ingredientId: 15,
+		ingredientId: 16,
 		price: 4000,
 		maxQuantity: 5
 	},
 	[Ingredient.ENERGIE_BOIS]: {
 		name: 'energie_bois',
-		ingredientId: 16,
+		ingredientId: 17,
 		price: 4000,
 		maxQuantity: 5
 	},
 	[Ingredient.SILEX_TAILLE]: {
 		name: 'silex_taille',
-		ingredientId: 17,
+		ingredientId: 18,
 		price: 150,
 		maxQuantity: 50
 	},
 	[Ingredient.FRAGMENT_DE_TEXTE_ANCIEN]: {
 		name: 'fragment_de_texte_ancien',
-		ingredientId: 18,
+		ingredientId: 19,
 		price: 1000,
 		maxQuantity: 20
 	},
 	[Ingredient.VIEIL_ANNEAU_PRECIEUX]: {
 		name: 'vieil_anneau_precieux',
-		ingredientId: 19,
+		ingredientId: 20,
 		price: 8000,
 		maxQuantity: 5
 	},
 	[Ingredient.CALICE_CISELE]: {
 		name: 'calice_cisele',
-		ingredientId: 20,
+		ingredientId: 21,
 		price: 8000,
 		maxQuantity: 5
 	},
 	[Ingredient.COLLIER_KARAT]: {
 		name: 'collier_karat',
-		ingredientId: 21,
+		ingredientId: 22,
 		price: 8000,
 		maxQuantity: 5
 	},
 	[Ingredient.BROCHE_EN_PARFAIT_ETAT]: {
 		name: 'broche_en_parfait_etat',
-		ingredientId: 22,
+		ingredientId: 23,
 		price: 30000,
 		maxQuantity: 5
 	},
 	[Ingredient.SUPERBE_COURONNE_ROYALE]: {
 		name: 'superbe_couronne_royale',
-		ingredientId: 23,
+		ingredientId: 24,
 		price: 40000,
 		maxQuantity: 5
 	},
 	[Ingredient.BRAS_MECANIQUE]: {
 		name: 'bras_mecanique',
-		ingredientId: 34,
+		ingredientId: 25,
 		price: 23000,
 		maxQuantity: 5
 	},
 	[Ingredient.FEUILLES_DE_PELINAE]: {
 		name: 'feuilles_de_pelinae',
-		ingredientId: 24,
+		ingredientId: 26,
 		price: 75,
 		maxQuantity: 50
 	},
 	[Ingredient.BOLET_PHALISK_BLANC]: {
 		name: 'bolet_phalisk_blanc',
-		ingredientId: 25,
+		ingredientId: 27,
 		price: 130,
 		maxQuantity: 20
 	},
 	[Ingredient.ORCHIDEE_FANTASQUE]: {
 		name: 'orchidee_fantasque',
-		ingredientId: 26,
+		ingredientId: 28,
 		price: 230,
 		maxQuantity: 5
 	},
 	[Ingredient.RACINE_DE_FIGONICIA]: {
 		name: 'racine_de_figonicia',
-		ingredientId: 27,
+		ingredientId: 29,
 		price: 230,
 		maxQuantity: 5
 	},
 	[Ingredient.SADIQUAE_MORDICUS]: {
 		name: 'sadiquae_mordicus',
-		ingredientId: 28,
+		ingredientId: 30,
 		price: 230,
 		maxQuantity: 5
 	},
 	[Ingredient.FLAUREOLE]: {
 		name: 'flaureole',
-		ingredientId: 29,
+		ingredientId: 31,
 		price: 500,
 		maxQuantity: 5
 	},
 	[Ingredient.SPORE_ETHERAL]: {
 		name: 'spore_etheral',
-		ingredientId: 30,
+		ingredientId: 32,
 		price: 150,
 		maxQuantity: 20
 	},
 	[Ingredient.POUSSE_SOMBRE]: {
 		name: 'pousse_sombre',
-		ingredientId: 31,
+		ingredientId: 33,
 		price: 300,
 		maxQuantity: 20
 	},
 	[Ingredient.GRAINE_DE_DEVOREUSE]: {
 		name: 'graine_de_devoreuse',
-		ingredientId: 32,
+		ingredientId: 34,
 		price: 15000,
 		maxQuantity: 100
 	}
