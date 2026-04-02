ef2a1a1c332ded1bcf952f047805cf84cc21f7f3
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-04-02 23:00:57 +0200

    feat(client): add dialog service

---
+++ app/client/src/services/dialog.service.ts
@@ -0,0 +1,45 @@
+import type { DialogPhaseResponse } from '@dinorpg/core/models/dialogs/dialogResponse.js';
+
+import { http } from '../utils/http.js';
+
+export type AvailableDialogSummary = {
+	id: string;
+	name: string;
+	place: number;
+	pnj: {
+		gfx: string;
+		image: boolean;
+		frame: string;
+		background: string;
+	};
+};
+
+export const DialogService = {
+	async getAvailableDialogs(dinozId: number): Promise<AvailableDialogSummary[]> {
+		return http()
+			.get(`/dialog?dinozId=${dinozId}`)
+			.then(res => Promise.resolve(res.data))
+			.catch(err => Promise.reject(err));
+	},
+
+	async startDialog(dinozId: number, dialogId: string): Promise<DialogPhaseResponse> {
+		return http()
+			.post(`/dialog/${dialogId}/start?dinozId=${dinozId}`)
+			.then(res => Promise.resolve(res.data))
+			.catch(err => Promise.reject(err));
+	},
+
+	async selectDialogLink(
+		dinozId: number,
+		dialogId: string,
+		linkId: string,
+		phaseId: string
+	): Promise<DialogPhaseResponse> {
+		return http()
+			.post(`/dialog/${dialogId}/links/${linkId}?dinozId=${dinozId}`, {
+				phaseId
+			})
+			.then(res => Promise.resolve(res.data))
+			.catch(err => Promise.reject(err));
+	}
+};
