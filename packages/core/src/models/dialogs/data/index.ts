2a80947a46fc047800556de1caaf9f317804d949
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-04-04 22:25:05 +0200

    feat(dialog): add coral miner NPC dialog'

--- packages/core/src/models/dialogs/data/index.ts
+++ packages/core/src/models/dialogs/data/index.ts
@@ -1,4 +1,5 @@
+import { coralMinerDialog } from './coralMiner.dialog.js';
 import { professorEugeneDialog } from './prof.dialog.js';
 import { testDialog } from './test.dialog.js';
 
-export const dialogDefinitions = [professorEugeneDialog, testDialog];
+export const dialogDefinitions = [professorEugeneDialog, coralMinerDialog, testDialog];
