d0892a796ba58fc4c9ae1df79b347a46a580a698
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-04-04 13:13:11 +0200

    feat(fight): add special dialog fight endpoint

--- packages/core/src/models/dialogs/dialogSpecial.ts
+++ packages/core/src/models/dialogs/dialogSpecial.ts
@@ -1,3 +1,5 @@
+import { MonsterFiche } from '../monster/monsterFiche.js';
+
 export type DialogSpecial =
 	| { type: 'none' }
 	| { type: 'fight'; monsters: string[]; friends: string[]; background?: string }
@@ -6,6 +8,6 @@ export type DialogSpecial =
 	| { type: 'useItem'; itemId: number; count: number }
 	| { type: 'useIngredient'; ingredientId: number; count: number }
 	| { type: 'useGold'; amount: number }
-	| { type: 'startFight' }
+	| { type: 'startFight'; fightId: MonsterFiche[] }
 	| { type: 'popup' }
 	| { type: 'status'; status: string };
