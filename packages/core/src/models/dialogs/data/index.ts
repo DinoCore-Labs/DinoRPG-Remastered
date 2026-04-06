6f353d277602879c65ea90ba4e6d120920726431
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-04-06 15:09:13 +0200

    feat(dialog): add Padamoine dialog

--- packages/core/src/models/dialogs/data/index.ts
+++ packages/core/src/models/dialogs/data/index.ts
@@ -2,6 +2,7 @@ import { atlanteGuardDialog } from './atlanteGuard.dialog.js';
 import { coralMinerDialog } from './coralMiner.dialog.js';
 import { hydargolDialog } from './hydargol.dialog.js';
 import { jovebozeDialog } from './joveboze.dialog.js';
+import { padamoineDialog } from './padamoine.dialog.js';
 import { professorEugeneDialog } from './prof.dialog.js';
 import { testDialog } from './test.dialog.js';
 
@@ -9,6 +10,7 @@ export const dialogDefinitions = [
 	// Dinoville
 	professorEugeneDialog,
 	jovebozeDialog,
+	padamoineDialog,
 	// Iles Atlantéinées
 	coralMinerDialog,
 	atlanteGuardDialog,
