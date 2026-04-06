b0cad49ef40982708771f885c8cc4b9d7a0d14c1
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-04-06 14:03:50 +0200

    feat(dialog): add Joveboze dialog

---
+++ packages/core/src/models/dialogs/data/joveboze.dialog.ts
@@ -0,0 +1,105 @@
+import { PlaceEnum } from '../../enums/PlaceEnum.js';
+import { bossList } from '../../monster/bossList.js';
+import { parseCondition } from '../../utils/conditions/parseConditions.js';
+import { DialogDefinition } from '../dialog.js';
+
+export const jovebozeDialog: DialogDefinition = {
+	id: 'joveboze',
+	name: 'npc.joveboze.name',
+	place: PlaceEnum.PORT_DE_PRECHE,
+	first: 'begin',
+	cond: parseCondition('fx(jvbz)'),
+	pnj: {
+		image: false,
+		gfx: 'joveboze',
+		frame: 'speak',
+		background: '1'
+	},
+	phases: {
+		begin: {
+			id: 'begin',
+			text: 'npc.joveboze.dialog.begin',
+			fast: false,
+			next: ['trad', 'sry']
+		},
+		trad: {
+			id: 'trad',
+			text: 'npc.joveboze.dialog.trad',
+			fast: true,
+			next: []
+		},
+		sry: {
+			id: 'sry',
+			text: 'npc.joveboze.dialog.sry',
+			fast: false,
+			next: ['go', 'trad']
+		},
+		go: {
+			id: 'go',
+			text: 'npc.joveboze.dialog.go',
+			fast: true,
+			next: ['attack', 'back']
+		},
+		attack: {
+			id: 'attack',
+			name: 'npc.joveboze.name',
+			text: '',
+			fast: true,
+			next: [],
+			special: [
+				{
+					type: 'startFight',
+					fightId: [bossList.RASCAPHANDRE]
+				}
+			],
+			pnj: {
+				image: false,
+				gfx: 'joveboze',
+				frame: 'speak',
+				background: 'rasca'
+			}
+		},
+		attack_win: {
+			id: 'attack_win',
+			text: 'npc.joveboze.dialog.attack',
+			effects: [
+				{
+					type: 'noEffect',
+					effect: 'jvbz'
+				},
+				{
+					type: 'effect',
+					effect: 'rasca'
+				}
+			],
+			next: []
+		}
+	},
+	links: {
+		sry: {
+			id: 'sry',
+			text: 'npc.joveboze.link.sry',
+			target: 'sry'
+		},
+		trad: {
+			id: 'trad',
+			text: 'npc.joveboze.link.trad',
+			target: 'trad'
+		},
+		go: {
+			id: 'go',
+			text: 'npc.joveboze.link.go',
+			target: 'go'
+		},
+		attack: {
+			id: 'attack',
+			text: 'npc.joveboze.link.attack',
+			target: 'attack'
+		},
+		back: {
+			id: 'back',
+			text: 'npc.joveboze.link.back',
+			target: 'attack'
+		}
+	}
+};
