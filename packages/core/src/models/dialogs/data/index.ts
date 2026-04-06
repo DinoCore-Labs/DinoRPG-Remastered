228ec80ff4ab45993001ecda5243acaf9da361cd
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-04-06 13:46:48 +0200

    feat(dialog): support admin condition based on user role

--- packages/core/src/models/dialogs/data/index.ts
+++ packages/core/src/models/dialogs/data/index.ts
@@ -1,5 +1,6 @@
+import { atlanteGuardDialog } from './atlanteGuard.dialog.js';
 import { coralMinerDialog } from './coralMiner.dialog.js';
 import { professorEugeneDialog } from './prof.dialog.js';
 import { testDialog } from './test.dialog.js';
 
-export const dialogDefinitions = [professorEugeneDialog, coralMinerDialog, testDialog];
+export const dialogDefinitions = [professorEugeneDialog, coralMinerDialog, atlanteGuardDialog, testDialog];
