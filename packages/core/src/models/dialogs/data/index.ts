3deceaae86db8f85aeb33fce50239c7e519917b4
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-04-06 14:03:50 +0200

    feat(dialog): add Joveboze dialog

--- packages/core/src/models/dialogs/data/index.ts
+++ packages/core/src/models/dialogs/data/index.ts
@@ -1,6 +1,13 @@
 import { atlanteGuardDialog } from './atlanteGuard.dialog.js';
 import { coralMinerDialog } from './coralMiner.dialog.js';
+import { jovebozeDialog } from './joveboze.dialog.js';
 import { professorEugeneDialog } from './prof.dialog.js';
 import { testDialog } from './test.dialog.js';
 
-export const dialogDefinitions = [professorEugeneDialog, coralMinerDialog, atlanteGuardDialog, testDialog];
+export const dialogDefinitions = [
+	professorEugeneDialog,
+	coralMinerDialog,
+	atlanteGuardDialog,
+	jovebozeDialog,
+	testDialog
+];
