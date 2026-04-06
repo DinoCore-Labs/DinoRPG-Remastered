3056d9cc379778fbdda2f94b36160a1c6f70f088
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-04-06 14:50:47 +0200

    feat(dialog): add Hydargol dialog

--- packages/core/src/models/dialogs/data/index.ts
+++ packages/core/src/models/dialogs/data/index.ts
@@ -1,13 +1,17 @@
 import { atlanteGuardDialog } from './atlanteGuard.dialog.js';
 import { coralMinerDialog } from './coralMiner.dialog.js';
+import { hydargolDialog } from './hydargol.dialog.js';
 import { jovebozeDialog } from './joveboze.dialog.js';
 import { professorEugeneDialog } from './prof.dialog.js';
 import { testDialog } from './test.dialog.js';
 
 export const dialogDefinitions = [
+	// Dinoville
 	professorEugeneDialog,
+	jovebozeDialog,
+	// Iles Atlantéinées
 	coralMinerDialog,
 	atlanteGuardDialog,
-	jovebozeDialog,
+	hydargolDialog,
 	testDialog
 ];
