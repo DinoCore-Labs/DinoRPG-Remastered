aecc13d7a638e3c472157e155821aed3fbcf130c
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-04-06 20:06:24 +0200

    feat(i18n): add weird man dialog

--- packages/core/src/models/dialogs/data/index.ts
+++ packages/core/src/models/dialogs/data/index.ts
@@ -6,6 +6,7 @@ import { jovebozeDialog } from './joveboze.dialog.js';
 import { padamoineDialog } from './padamoine.dialog.js';
 import { professorEugeneDialog } from './prof.dialog.js';
 import { testDialog } from './test.dialog.js';
+import { weirdManDialog } from './weirdMan.dialog.js';
 
 export const dialogDefinitions = [
 	// Dinoville
@@ -17,6 +18,8 @@ export const dialogDefinitions = [
 	atlanteGuardDialog,
 	hydargolDialog,
 	archisageDialog,
+	// Foret
+	weirdManDialog,
 	// Test
 	testDialog
 ];
