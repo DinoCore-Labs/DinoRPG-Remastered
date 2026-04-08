27bda0a7fb5e6cd660521107c5ffee823ac247ce
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-04-08 17:57:22 +0200

    feat(dialog): add Papy Joe dialog

---
+++ packages/core/src/models/dialogs/data/papy.dialog.ts
@@ -0,0 +1,32 @@
+import { PlaceEnum } from '../../enums/PlaceEnum.js';
+import { DialogDefinition } from '../dialog.js';
+
+export const papyJoeDialog: DialogDefinition = {
+	id: 'papy_joe',
+	place: PlaceEnum.PAPY_JOE,
+	name: 'npc.papyJoe.name',
+	pnj: {
+		image: false,
+		gfx: 'papy',
+		frame: 'speak',
+		background: '1'
+	},
+	phases: {
+		begin: {
+			id: 'begin',
+			text: 'npc.papyJoe.dialog.begin',
+			next: ['missions']
+		},
+		missions: {
+			id: 'missions',
+			text: '',
+			next: []
+		}
+	},
+	links: {
+		missions: {
+			id: 'missions',
+			text: 'npc.papyJoe.choice.missions'
+		}
+	}
+};
