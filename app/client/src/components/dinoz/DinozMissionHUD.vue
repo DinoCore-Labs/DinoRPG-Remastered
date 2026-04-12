685b1dea49c7cf80e02358986a75014538cbb193
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-04-12 23:58:27 +0200

    fix(client): ts errors build again again

--- app/client/src/components/dinoz/DinozMissionHUD.vue
+++ app/client/src/components/dinoz/DinozMissionHUD.vue
@@ -223,7 +223,7 @@ export default defineComponent({
 					return false;
 			}
 		},
-		getGoalText(goal: MissionGoal, tracking: number, mode?: GoalTextMode): string | null {
+		getGoalText(goal: MissionGoal, tracking: number, _mode: GoalTextMode): string | null {
 			switch (goal.type) {
 				case 'AT': {
 					if (goal.titleKey) {
