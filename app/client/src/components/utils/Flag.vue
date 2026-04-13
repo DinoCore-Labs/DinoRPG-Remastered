c17fcbedeee7ebfb5b0b68c6b6a05a79cc73b3a5
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-04-13 15:55:38 +0200

    feat(i18n): add DZLocale component for player language switching

---
+++ app/client/src/components/utils/Flag.vue
@@ -0,0 +1,18 @@
+<template>
+	<img :src="getImgURL('lang', `lang_${lang}`)" class="flag" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'Flag',
+	props: {
+		lang: {
+			type: String
+		}
+	}
+});
+</script>
+
+<style scoped lang="scss"></style>
