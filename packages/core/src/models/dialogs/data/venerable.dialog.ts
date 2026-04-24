39c0158df7df3737394659b79a8f10e5e84f0333
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-04-24 22:06:02 +0200

    feat(i18n): add venerable

--- packages/core/src/models/dialogs/data/venerable.dialog.ts
+++ packages/core/src/models/dialogs/data/venerable.dialog.ts
@@ -21,31 +21,31 @@ export const venerableDialog = defineDialog({
 	phases: {
 		begin: {
 			id: 'begin',
-			text: 'npc.venerable.dialog.begin',
+			text: 'npc.venerable.intro.dialog.begin',
 			next: ['moi', 'moi2', 'bye']
 		},
 
 		bye: {
 			id: 'bye',
-			text: 'npc.venerable.dialog.bye',
+			text: 'npc.venerable.intro.dialog.bye',
 			fast: true
 		},
 
 		moi: {
 			id: 'moi',
-			text: 'npc.venerable.dialog.moi',
+			text: 'npc.venerable.intro.dialog.moi',
 			next: ['perdu', 'chasse', 'deal', 'korgon', 'auto']
 		},
 
 		korgon: {
 			id: 'korgon',
-			text: 'npc.venerable.dialog.korgon',
+			text: 'npc.venerable.intro.dialog.korgon',
 			next: ['next0', 'next1']
 		},
 
 		next1: {
 			id: 'next1',
-			text: 'npc.venerable.dialog.next1',
+			text: 'npc.venerable.intro.dialog.next1',
 			next: ['next2'],
 			effects: [{ type: 'effect', effect: 'vmem' }]
 		},
@@ -58,7 +58,7 @@ export const venerableDialog = defineDialog({
 
 		fight_win: {
 			id: 'fight_win',
-			text: 'npc.venerable.dialog.fight',
+			text: 'npc.venerable.intro.dialog.fight',
 			effects: [
 				{ type: 'collection', collection: 'vener' },
 				{ type: 'effect', effect: 'vmem' },
@@ -68,117 +68,117 @@ export const venerableDialog = defineDialog({
 
 		moi2: {
 			id: 'moi2',
-			text: 'npc.venerable.dialog.moi2',
+			text: 'npc.venerable.intro.dialog.moi2',
 			next: ['question']
 		},
 
 		question: {
 			id: 'question',
-			text: 'npc.venerable.dialog.question',
+			text: 'npc.venerable.intro.dialog.question',
 			next: ['force', 'fight', 'fight2']
 		},
 
 		force: {
 			id: 'force',
-			text: 'npc.venerable.dialog.force',
+			text: 'npc.venerable.intro.dialog.force',
 			effects: [{ type: 'effect', effect: 'fflag' }]
 		},
 
 		fight2: {
 			id: 'fight2',
-			text: 'npc.venerable.dialog.fight2'
+			text: 'npc.venerable.intro.dialog.fight2'
 		}
 	},
 	links: {
 		moi: {
 			id: 'moi',
-			text: 'npc.venerable.choice.moi',
+			text: 'npc.venerable.intro.choice.moi',
 			cond: parseCondition('!fx(vmem)')
 		},
 
 		moi2: {
 			id: 'moi2',
-			text: 'npc.venerable.choice.moi2',
+			text: 'npc.venerable.intro.choice.moi2',
 			cond: parseCondition('fx(vmem)')
 		},
 
 		bye: {
 			id: 'bye',
-			text: 'npc.venerable.choice.bye'
+			text: 'npc.venerable.intro.choice.bye'
 		},
 
 		perdu: {
 			id: 'perdu',
-			text: 'npc.venerable.choice.perdu',
+			text: 'npc.venerable.intro.choice.perdu',
 			target: 'fight'
 		},
 
 		chasse: {
 			id: 'chasse',
-			text: 'npc.venerable.choice.chasse',
+			text: 'npc.venerable.intro.choice.chasse',
 			target: 'fight'
 		},
 
 		deal: {
 			id: 'deal',
-			text: 'npc.venerable.choice.deal',
+			text: 'npc.venerable.intro.choice.deal',
 			target: 'fight'
 		},
 
 		korgon: {
 			id: 'korgon',
-			text: 'npc.venerable.choice.korgon'
+			text: 'npc.venerable.intro.choice.korgon'
 		},
 
 		auto: {
 			id: 'auto',
-			text: 'npc.venerable.choice.auto',
+			text: 'npc.venerable.intro.choice.auto',
 			target: 'fight'
 		},
 
 		next0: {
 			id: 'next0',
-			text: 'npc.venerable.choice.next0',
+			text: 'npc.venerable.intro.choice.next0',
 			target: 'fight'
 		},
 
 		next1: {
 			id: 'next1',
-			text: 'npc.venerable.choice.next1'
+			text: 'npc.venerable.intro.choice.next1'
 		},
 
 		next2: {
 			id: 'next2',
-			text: 'npc.venerable.choice.next2',
+			text: 'npc.venerable.intro.choice.next2',
 			target: 'question'
 		},
 
 		question: {
 			id: 'question',
-			text: 'npc.venerable.choice.question'
+			text: 'npc.venerable.intro.choice.question'
 		},
 
 		force: {
 			id: 'force',
-			text: 'npc.venerable.choice.force'
+			text: 'npc.venerable.intro.choice.force'
 		},
 
 		fight: {
 			id: 'fight',
-			text: 'npc.venerable.choice.fight',
+			text: 'npc.venerable.intro.choice.fight',
 			cond: parseCondition('!fx(vkill)')
 		},
 
 		fight2: {
 			id: 'fight2',
-			text: 'npc.venerable.choice.fight2',
+			text: 'npc.venerable.intro.choice.fight2',
 			cond: parseCondition('fx(vkill)')
 		}
 	}
 });
 
 export const venerableHurtDialog = defineDialog({
-	id: 'venerable_hurt',
+	id: 'venerable_.hurt',
 	place: PlaceEnum.REPAIRE_DU_VENERABLE,
 	name: 'npc.venerable.name',
 	pnj: {
@@ -190,19 +190,19 @@ export const venerableHurtDialog = defineDialog({
 	phases: {
 		begin: {
 			id: 'begin',
-			text: 'npc.venerableHurt.dialog.begin',
+			text: 'npc.venerable.hurt.dialog.begin',
 			next: ['ok', 'ko', 'ko2', 'force2']
 		},
 
 		ok: {
 			id: 'ok',
-			text: 'npc.venerableHurt.dialog.ok',
+			text: 'npc.venerable.hurt.dialog.ok',
 			fast: true
 		},
 
 		ko: {
 			id: 'ko',
-			text: 'npc.venerableHurt.dialog.ko',
+			text: 'npc.venerable.hurt.dialog.ko',
 			next: ['comb']
 		},
 
@@ -214,7 +214,7 @@ export const venerableHurtDialog = defineDialog({
 
 		comb_win: {
 			id: 'comb_win',
-			text: 'npc.venerableHurt.dialog.comb',
+			text: 'npc.venerable.hurt.dialog.comb',
 			effects: [
 				{ type: 'collection', collection: 'vener' },
 				{ type: 'effect', effect: 'vmem' },
@@ -224,41 +224,41 @@ export const venerableHurtDialog = defineDialog({
 
 		force2: {
 			id: 'force2',
-			text: 'npc.venerableHurt.dialog.force2',
+			text: 'npc.venerable.hurt.dialog.force2',
 			effects: [{ type: 'effect', effect: 'fflag' }]
 		},
 
 		ko2: {
 			id: 'ko2',
-			text: 'npc.venerableHurt.dialog.ko2'
+			text: 'npc.venerable.hurt.dialog.ko2'
 		}
 	},
 	links: {
 		ok: {
 			id: 'ok',
-			text: 'npc.venerableHurt.choice.ok'
+			text: 'npc.venerable.hurt.choice.ok'
 		},
 
 		ko: {
 			id: 'ko',
-			text: 'npc.venerableHurt.choice.ko',
+			text: 'npc.venerable.hurt.choice.ko',
 			cond: parseCondition('!fx(vmem)')
 		},
 
 		ko2: {
 			id: 'ko2',
-			text: 'npc.venerableHurt.choice.ko2',
+			text: 'npc.venerable.hurt.choice.ko2',
 			cond: parseCondition('fx(vmem)')
 		},
 
 		force2: {
 			id: 'force2',
-			text: 'npc.venerableHurt.choice.force2'
+			text: 'npc.venerable.hurt.choice.force2'
 		},
 
 		comb: {
 			id: 'comb',
-			text: 'npc.venerableHurt.choice.comb',
+			text: 'npc.venerable.hurt.choice.comb',
 			cond: parseCondition('!fx(vkill)')
 		}
 	}
