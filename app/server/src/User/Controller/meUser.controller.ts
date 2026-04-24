28c4f653ca8235adc0197eb91bd0f9b0f3d47b13
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-04-24 18:15:17 +0200

    refactor(core): make dinoz ordering utility backend-friendly

--- app/server/src/User/Controller/meUser.controller.ts
+++ app/server/src/User/Controller/meUser.controller.ts
@@ -1,7 +1,7 @@
 import { StatTracking } from '@dinorpg/core/models/enums/StatsTracking.js';
 import { Item } from '@dinorpg/core/models/items/itemList.js';
 import { Skill } from '@dinorpg/core/models/skills/skillList.js';
-import { getMaxXp } from '@dinorpg/core/utils/dinozUtils.js';
+import { getMaxXp, orderDinozList } from '@dinorpg/core/utils/dinozUtils.js';
 import { FastifyReply, FastifyRequest } from 'fastify';
 
 import { addItemToInventory } from '../../Inventory/Controller/addItem.controller.js';
@@ -165,10 +165,12 @@ export async function meUser(req: FastifyRequest, reply: FastifyReply) {
 			treasureTicket,
 			priest: user.priest,
 			shopKeeper: user.shopKeeper,
-			dinoz: user.dinoz.map(dinoz => ({
-				...dinoz,
-				maxExperience: getMaxXp(dinoz)
-			})),
+			dinoz: orderDinozList(
+				user.dinoz.map(dinoz => ({
+					...dinoz,
+					maxExperience: getMaxXp(dinoz)
+				}))
+			),
 			rewards: user.rewards.map(reward => reward.rewardId)
 		});
 	} catch (err) {
