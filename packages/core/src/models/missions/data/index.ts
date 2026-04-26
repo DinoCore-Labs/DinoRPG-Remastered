d721b913435b26df4771ce250cee2ff45928c3ff
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-04-26 20:53:33 +0200

    fix(missions): register Bao Bob missions in mission list

--- packages/core/src/models/missions/data/index.ts
+++ packages/core/src/models/missions/data/index.ts
@@ -1,6 +1,12 @@
 import type { MissionDefinition } from '../mission.js';
+import { baoBobMissions } from './baoBob.missions.js';
 import { papyJoeMissions } from './papyJoe.missions.js';
 import { rodeurMissions } from './rodeur.missions.js';
 import { shamanMouMissions } from './shamanMou.missions.js';
 
-export const missionList: MissionDefinition[] = [...papyJoeMissions, ...shamanMouMissions, ...rodeurMissions];
+export const missionList: MissionDefinition[] = [
+	...papyJoeMissions,
+	...shamanMouMissions,
+	...rodeurMissions,
+	...baoBobMissions
+];
