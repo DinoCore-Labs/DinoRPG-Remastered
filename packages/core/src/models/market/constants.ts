0512065d57bfb5f1f694c1fdc1addd38e399efb1
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-05-09 21:57:34 +0200

    chore: add breeder market constants

---
+++ packages/core/src/models/market/constants.ts
@@ -0,0 +1,5 @@
+export const MARKET_OFFER_DURATION_MS = 24 * 60 * 60 * 1000;
+export const MARKET_BID_EXTENSION_MS = 30 * 1000;
+
+export const MARKET_EXPIRATION_JOB_KEY = 'market-offer-expiration';
+export const MARKET_EXPIRATION_INTERVAL_MS = 1_000;
