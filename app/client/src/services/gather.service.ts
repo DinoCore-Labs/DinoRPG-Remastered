00d6900361f4f405d44506e58c6156b2c2c3152a
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-05-10 14:23:55 +0200

    refactor: migrate gather service to API helpers

--- app/client/src/services/gather.service.ts
+++ app/client/src/services/gather.service.ts
@@ -1,26 +1,16 @@
 import type { GatherPublicGrid } from '@dinorpg/core/models/gather/gatherPublicGrid.js';
 import type { GatherResult } from '@dinorpg/core/models/gather/gatherResult.js';
 
-import { http } from '../utils/http';
+import { api } from '../utils/http';
 
 export const GatherService = {
-	async getGatherGrid(dinozId: number, gridType: string): Promise<GatherPublicGrid> {
-		return http()
-			.get(`/gather/grid/${gridType}/${dinozId}`)
-			.then(res => Promise.resolve(res.data))
-			.catch(err => {
-				Promise.reject(err);
-			});
+	getGatherGrid(dinozId: number, gridType: string): Promise<GatherPublicGrid> {
+		return api.get<GatherPublicGrid>(`/gather/grid/${gridType}/${dinozId}`);
 	},
-	async gatherWithDinoz(dinozId: number, gridType: string, box: number[][]): Promise<GatherResult> {
-		return http()
-			.put(`/gather/${dinozId}`, {
-				type: gridType,
-				box: box
-			})
-			.then(res => Promise.resolve(res.data))
-			.catch(err => {
-				Promise.reject(err);
-			});
+	gatherWithDinoz(dinozId: number, gridType: string, box: number[][]): Promise<GatherResult> {
+		return api.put<GatherResult>(`/gather/${dinozId}`, {
+			type: gridType,
+			box
+		});
 	}
 };
