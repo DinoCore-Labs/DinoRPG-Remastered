2b0e6de0e4f3ed6a95f89bf86b0d098dc98bcefc
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-05-10 14:21:14 +0200

    refactor: migrate level service to API helpers

--- app/client/src/services/level.service.ts
+++ app/client/src/services/level.service.ts
@@ -1,22 +1,16 @@
 import type { DinozSkillOwnAndUnlockable } from '@dinorpg/core/models/skills/dinozSkillOwnAndUnlockable.js';
 import type { LearnSkillData } from '@dinorpg/core/models/skills/learnSkillData.js';
 
-import { http } from '../utils/http';
+import { api } from '../utils/http';
 
 export const LevelService = {
-	async levelUp(dinozId: number, tryNumber: string): Promise<DinozSkillOwnAndUnlockable> {
-		return http()
-			.get(`/level/learnableskills/${dinozId}/${tryNumber}`)
-			.then(res => Promise.resolve(res.data))
-			.catch(err => Promise.reject(err));
+	levelUp(dinozId: number, tryNumber: string): Promise<DinozSkillOwnAndUnlockable> {
+		return api.get<DinozSkillOwnAndUnlockable>(`/level/learnableskills/${dinozId}/${tryNumber}`);
 	},
-	async learnSkill(dinozId: number, skillIdList: Array<number>, tryNumber: number): Promise<LearnSkillData> {
-		return http()
-			.post(`/level/learnskill/${dinozId}`, {
-				skillIdList: skillIdList,
-				tryNumber: tryNumber
-			})
-			.then(res => Promise.resolve(res.data))
-			.catch(err => Promise.reject(err));
+	learnSkill(dinozId: number, skillIdList: number[], tryNumber: number): Promise<LearnSkillData> {
+		return api.post<LearnSkillData>(`/level/learnskill/${dinozId}`, {
+			skillIdList,
+			tryNumber
+		});
 	}
 };
