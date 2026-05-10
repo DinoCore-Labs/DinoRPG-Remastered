2b360b0e63620ab8c0e433d4425f523a9cc142d0
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-05-10 14:40:25 +0200

    refactor: migrate admin jobs service to API helpers

--- app/client/src/services/adminJobs.service.ts
+++ app/client/src/services/adminJobs.service.ts
@@ -1,24 +1,21 @@
 import type { JobDefinition, JobRun } from '@dinorpg/core/models/admin/adminJobs.js';
 
-import { http } from '../utils/http';
+import { api } from '../utils/http';
+
+type OkResponse = {
+	ok: boolean;
+};
+
+const getAdminJobPath = (key: string): string => `/admin/jobs/${encodeURIComponent(key)}`;
 
 export const AdminJobsService = {
 	list(): Promise<JobDefinition[]> {
-		return http()
-			.get('/admin/jobs')
-			.then(res => res.data)
-			.catch(err => Promise.reject(err));
+		return api.get<JobDefinition[]>('/admin/jobs');
 	},
 	getRuns(key: string): Promise<JobRun[]> {
-		return http()
-			.get(`/admin/jobs/${encodeURIComponent(key)}/runs`)
-			.then(res => res.data)
-			.catch(err => Promise.reject(err));
+		return api.get<JobRun[]>(`${getAdminJobPath(key)}/runs`);
 	},
-	runNow(key: string): Promise<{ ok: boolean }> {
-		return http()
-			.post(`/admin/jobs/${encodeURIComponent(key)}/run`)
-			.then(res => res.data)
-			.catch(err => Promise.reject(err));
+	runNow(key: string): Promise<OkResponse> {
+		return api.post<OkResponse>(`${getAdminJobPath(key)}/run`);
 	}
 };
