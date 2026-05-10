323192acbd0b2897425f0b0942938d1bd57c620c
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-05-10 14:14:27 +0200

    refactor: migrate training center service to API helpers

--- app/client/src/services/trainingCenter.service.ts
+++ app/client/src/services/trainingCenter.service.ts
@@ -1,13 +1,10 @@
 import type { FightResult } from '@dinorpg/core/models/fight/fightResult.js';
 import type { TrainingCenterProgramKey } from '@dinorpg/core/models/trainingCenter/trainingCenter.js';
 
-import { http } from '../utils/http';
+import { api } from '../utils/http';
 
 export const TrainingCenterService = {
-	async startTrainingCenterFight(dinozId: number, program: TrainingCenterProgramKey): Promise<FightResult> {
-		return http()
-			.post(`/cef/${dinozId}/training-center/start`, { program })
-			.then(res => Promise.resolve(res.data))
-			.catch(err => Promise.reject(err));
+	startTrainingCenterFight(dinozId: number, program: TrainingCenterProgramKey): Promise<FightResult> {
+		return api.post<FightResult>(`/cef/${dinozId}/training-center/start`, { program });
 	}
 };
