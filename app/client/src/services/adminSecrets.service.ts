2afeb6ced2e72f2d432a2267efece914d061b66b
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-05-10 14:37:10 +0200

    refactor: migrate admin secret service to API helpers

--- app/client/src/services/adminSecrets.service.ts
+++ app/client/src/services/adminSecrets.service.ts
@@ -1,18 +1,16 @@
 import type { AdminSecret } from '@dinorpg/core/models/admin/adminSecrets.js';
 
-import { http } from '../utils/http';
+import { api } from '../utils/http';
+
+const getAdminSecretPath = (key: string): string => `/admin/secrets/${encodeURIComponent(key)}`;
 
 export const AdminSecretService = {
 	getSecrets(): Promise<AdminSecret[]> {
-		return http()
-			.get('/admin/secrets')
-			.then(res => res.data)
-			.catch(err => Promise.reject(err));
+		return api.get<AdminSecret[]>('/admin/secrets');
 	},
 	updateSecret(key: string, value: string): Promise<AdminSecret> {
-		return http()
-			.put(`/admin/secrets/${encodeURIComponent(key)}`, { value })
-			.then(res => res.data)
-			.catch(err => Promise.reject(err));
+		return api.put<AdminSecret>(getAdminSecretPath(key), {
+			value
+		});
 	}
 };
