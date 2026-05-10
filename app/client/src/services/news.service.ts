4a7f82f9189f7186b812dc3c84171ce0c6c67524
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-05-10 14:17:57 +0200

    refactor: migrate news service to API helpers

--- app/client/src/services/news.service.ts
+++ app/client/src/services/news.service.ts
@@ -1,25 +1,16 @@
 import type { PublicNewsListItem } from '@dinorpg/core/models/news/news.js';
 import type { ToggleNewsLikeResult, VotePollResult } from '@dinorpg/core/models/news/newsInput.js';
 
-import { http } from '../utils/http';
+import { api } from '../utils/http';
 
 export const NewsService = {
-	async getNewsPage(page: number): Promise<PublicNewsListItem[]> {
-		return http()
-			.get(`/news/page/${page}`)
-			.then(res => Promise.resolve(res.data))
-			.catch(err => Promise.reject(err));
+	getNewsPage(page: number): Promise<PublicNewsListItem[]> {
+		return api.get<PublicNewsListItem[]>(`/news/page/${page}`);
 	},
-	async toggleLike(newsId: number): Promise<ToggleNewsLikeResult> {
-		return http()
-			.post(`/news/${newsId}/like`)
-			.then(res => Promise.resolve(res.data))
-			.catch(err => Promise.reject(err));
+	toggleLike(newsId: number): Promise<ToggleNewsLikeResult> {
+		return api.post<ToggleNewsLikeResult>(`/news/${newsId}/like`);
 	},
-	async voteToPoll(pollId: number, optionId: number): Promise<VotePollResult> {
-		return http()
-			.post(`/news/polls/${pollId}/vote/${optionId}`)
-			.then(res => Promise.resolve(res.data))
-			.catch(err => Promise.reject(err));
+	voteToPoll(pollId: number, optionId: number): Promise<VotePollResult> {
+		return api.post<VotePollResult>(`/news/polls/${pollId}/vote/${optionId}`);
 	}
 };
