efbb0b3c2e1f76f20e92dade3edd46e45751ec02
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-05-21 21:20:11 +0200

    feat: highlight min max values in admin log charts

--- app/client/src/components/admin/AdminLogsChart.vue
+++ app/client/src/components/admin/AdminLogsChart.vue
@@ -1,5 +1,9 @@
 <template>
 	<div class="chart">
+		<div v-if="highlightExtremes && hasExtremes" class="legend">
+			<span class="legend__item legend__item--max">Plus haut : {{ maxValue }}</span>
+			<span class="legend__item legend__item--min">Plus bas : {{ minValue }}</span>
+		</div>
 		<VChart class="chart__canvas" :option="option" autoresize />
 	</div>
 </template>
@@ -14,17 +18,55 @@ import VChart from 'vue-echarts';
 
 use([CanvasRenderer, LineChart, BarChart, GridComponent, TooltipComponent, LegendComponent]);
 
+const MAX_COLOR = '#2f9e44';
+const MIN_COLOR = '#e03131';
+
 const props = withDefaults(
 	defineProps<{
 		title: string;
 		labels: string[];
 		values: number[];
 		type?: 'bar' | 'line';
+		highlightExtremes?: boolean;
 	}>(),
 	{
-		type: 'bar'
+		type: 'bar',
+		highlightExtremes: true
 	}
 );
+
+const maxValue = computed(() => Math.max(...props.values));
+const minValue = computed(() => Math.min(...props.values));
+
+const hasExtremes = computed(() => {
+	return props.values.length > 1 && maxValue.value !== minValue.value;
+});
+
+const seriesData = computed(() => {
+	if (!props.highlightExtremes || !hasExtremes.value) {
+		return props.values;
+	}
+
+	return props.values.map(value => {
+		if (value === maxValue.value) {
+			return {
+				value,
+				itemStyle: {
+					color: MAX_COLOR
+				}
+			};
+		}
+		if (value === minValue.value) {
+			return {
+				value,
+				itemStyle: {
+					color: MIN_COLOR
+				}
+			};
+		}
+		return value;
+	});
+});
 
 const option = computed(() => ({
 	tooltip: {
@@ -33,7 +75,7 @@ const option = computed(() => ({
 	grid: {
 		left: 40,
 		right: 20,
-		top: 30,
+		top: props.highlightExtremes ? 42 : 30,
 		bottom: 35
 	},
 	xAxis: {
@@ -47,7 +89,7 @@ const option = computed(() => ({
 		{
 			name: props.title,
 			type: props.type,
-			data: props.values,
+			data: seriesData.value,
 			smooth: props.type === 'line'
 		}
 	]
@@ -57,11 +99,35 @@ const option = computed(() => ({
 <style scoped lang="scss">
 .chart {
 	width: 100%;
-	height: 320px;
+	height: 350px;
 	margin-top: 12px;
 }
 .chart__canvas {
 	width: 100%;
-	height: 100%;
+	height: 320px;
+}
+.legend {
+	display: flex;
+	gap: 12px;
+	margin-bottom: 6px;
+	font-size: 9pt;
+	font-weight: bold;
+}
+.legend__item {
+	display: inline-flex;
+	align-items: center;
+	gap: 4px;
+	&::before {
+		content: '';
+		width: 10px;
+		height: 10px;
+		border-radius: 50%;
+	}
+}
+.legend__item--max::before {
+	background: #2f9e44;
+}
+.legend__item--min::before {
+	background: #e03131;
 }
 </style>
