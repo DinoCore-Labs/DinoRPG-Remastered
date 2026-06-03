44c35ced8deda98d1ae9b7941f91f046a7fb5d27
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-06-03 14:14:04 +0200

    feat(bank): add Dinoland bank exchange rate constants

---
+++ packages/core/src/models/bank/constants.ts
@@ -0,0 +1,23 @@
+export const BANK_TREASURE_TICKET_GOLD_VALUE = 1000;
+
+export const BANK_EXCHANGE_RATE_MIN_BPS = 9000;
+export const BANK_EXCHANGE_RATE_MAX_BPS = 12000;
+export const BANK_EXCHANGE_RATE_SCALE = 10000;
+
+export const BANK_EXCHANGE_RATE_SECRET_KEY = 'bank.treasureTicketExchangeRateBps';
+export const BANK_EXCHANGE_RATE_JOB_KEY = 'bank-refresh-treasure-ticket-rate';
+
+export function generateBankExchangeRateBps() {
+	return (
+		Math.floor(Math.random() * (BANK_EXCHANGE_RATE_MAX_BPS - BANK_EXCHANGE_RATE_MIN_BPS + 1)) +
+		BANK_EXCHANGE_RATE_MIN_BPS
+	);
+}
+
+export function getBankExchangeRateFromBps(rateBps: number) {
+	return rateBps / BANK_EXCHANGE_RATE_SCALE;
+}
+
+export function getTreasureTicketGoldValue(quantity: number, rateBps: number) {
+	return Math.round((quantity * BANK_TREASURE_TICKET_GOLD_VALUE * rateBps) / BANK_EXCHANGE_RATE_SCALE);
+}
