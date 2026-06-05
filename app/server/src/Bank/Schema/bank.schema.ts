258d6803fbcbf127f143463683ca319b4dea184f
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-06-05 16:21:17 +0200

    feat(bank): add savings validation schemas

--- app/server/src/Bank/Schema/bank.schema.ts
+++ app/server/src/Bank/Schema/bank.schema.ts
@@ -3,3 +3,12 @@ import { z } from 'zod';
 export const convertTreasureTicketsBodySchema = z.object({
 	quantity: z.coerce.number().int().positive()
 });
+
+export const createBankSavingBodySchema = z.object({
+	amount: z.coerce.number().int().positive(),
+	durationDays: z.coerce.number().int()
+});
+
+export const claimBankSavingParamsSchema = z.object({
+	id: z.string().uuid()
+});
