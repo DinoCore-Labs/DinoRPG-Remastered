075a735865de543166edaedea94bd2055d7127ee
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-06-09 15:46:45 +0200

    feat(bank): enable savings tab

--- app/client/src/pages/BankPage.vue
+++ app/client/src/pages/BankPage.vue
@@ -16,11 +16,12 @@
 			<li :class="tabSelected === 1 ? 'active' : ''">
 				<a @click="sStore.setTabAccount(1)">{{ $t('bank.tabs.conversion') }}</a>
 			</li>
-			<li :class="tabSelected === 2 ? 'active disabled' : 'disabled'">
-				<a @click="sStore.setTabAccount(2)">{{ $t('bank.tabs.todo') }}</a>
+			<li :class="tabSelected === 2 ? 'active' : ''">
+				<a @click="sStore.setTabAccount(2)">{{ $t('bank.tabs.savings') }}</a>
 			</li>
 		</ul>
 		<BankTreasureTicketExchange v-if="tabSelected === 1" />
+		<BankSavings v-if="tabSelected === 2" />
 	</div>
 </template>
 
@@ -28,6 +29,7 @@
 import { defineComponent } from 'vue';
 import TitleHeader from '../components/utils/TitleHeader.vue';
 import BankTreasureTicketExchange from '../components/bank/BankTreasureTicketExchange.vue';
+import BankSavings from '../components/bank/BankSavings.vue';
 import { sessionStore } from '../store/sessionStore.ts';
 
 export default defineComponent({
@@ -38,7 +40,8 @@ export default defineComponent({
 	},
 	components: {
 		TitleHeader,
-		BankTreasureTicketExchange
+		BankTreasureTicketExchange,
+		BankSavings
 	},
 	data() {
 		return {};
