f1771f3149bc77d53558a1759b4c5aaae5a1e5ba
Docalaco <louis.og31@gmail.com>
2026-06-11 16:43:44 +0200

    feat(clan): add clanStore and clan type to userStore client

---
+++ app/client/src/store/clanStore.ts
@@ -0,0 +1,50 @@
+import { Language } from '@dinorpg/core/models/config/language.js';
+import type { ClanStore } from '@dinorpg/core/models/store/clanStore.js';
+import { defineStore } from 'pinia';
+
+import { ClanService } from '../services/clan.service.js';
+import { userStore } from './userStore';
+
+export const clanStore = defineStore('clanStore', {
+	state: (): ClanStore => ({
+		clan: undefined,
+		myClan: undefined,
+		clanEvent: undefined
+	}),
+	getters: {
+		getClan: (state: ClanStore) => state.clan,
+		getMyclan: (state: ClanStore) => state.myClan,
+		getClanId: (state: ClanStore) => state.clan?.id ?? 0,
+		getOngoingEvent: (state: ClanStore) => {
+			if (state.clanEvent && state.clanEvent.endDate > new Date()) {
+				return state.clanEvent.id;
+			}
+		}
+	},
+	actions: {
+		async loadClan(clanId: number) {
+			this.clan = undefined;
+			this.clan = await ClanService.getClan(clanId);
+			if (this.clan.id === userStore().clanId) {
+				this.myClan = this.clan;
+			}
+		},
+		async updateLang(clanId: number, languages: Language[]) {
+			if (this.clan) {
+				const response = await ClanService.updateClanLangs(clanId, languages);
+				this.clan.languages.splice(0);
+				this.clan.languages.push(...response);
+			}
+		},
+		updateBanner(banner: string): void {
+			if (!this.clan) return;
+			this.clan.banner = banner;
+		},
+		setClanEvent(clanEvent: { id: string; endDate: Date } | undefined): void {
+			this.clanEvent = clanEvent;
+		}
+	},
+	persist: {
+		storage: window.sessionStorage
+	}
+});
