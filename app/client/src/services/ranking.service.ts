d134ac97fea40dda042f3fca8e0f78aff013747f
Docalaco <louis.og31@gmail.com>
2026-06-11 16:55:14 +0200

    feat(ranking): update ranking API client

--- app/client/src/services/ranking.service.ts
+++ app/client/src/services/ranking.service.ts
@@ -1,10 +1,17 @@
-import type { RankingEntry, RankingPositionResponse } from '@dinorpg/core/models/ranking/rankingEntry.js';
+import type {
+	ClanRankingEntry,
+	RankingEntry,
+	RankingPositionResponse
+} from '@dinorpg/core/models/ranking/rankingEntry.js';
 
 import { api } from '../utils/http';
 
 export const RankingService = {
 	getRanking(sort: string, page: number): Promise<RankingEntry[]> {
 		return api.get<RankingEntry[]>(`/ranking/list/${sort}/${page}`);
+	},
+	getClanRanking(sort: string, page: number): Promise<ClanRankingEntry[]> {
+		return api.get<ClanRankingEntry[]>(`/ranking/clan/${sort}/${page}`);
 	},
 	getPositionRanking(userId: string): Promise<RankingPositionResponse> {
 		return api.get<RankingPositionResponse>(`/ranking/position/${encodeURIComponent(userId)}`);
