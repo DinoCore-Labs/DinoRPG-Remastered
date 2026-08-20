1f38c815c587504ed60c327cb8f248f8bd6f5303
Docalaco <louis.og31@gmail.com>
2026-08-20 15:31:52 +0200

    feat(dojo): add dojo scenario

---
+++ packages/core/src/models/scenarios/data/dojoScenario.ts
@@ -0,0 +1,9 @@
+import { defineScenario } from '../scenario.js';
+
+export const dojoScenario = defineScenario({
+	key: 'dojo',
+	sid: 4,
+	nameKey: 'scenarios.dojo.name',
+	maxProgression: 1
+});
+export const DOJO_SCENARIO_KEY = 'dojo';
