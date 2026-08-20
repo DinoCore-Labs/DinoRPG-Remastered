9b6c1d1cd4192eeed5a3f80b40e81e7ed7693bec
Docalaco <louis.og31@gmail.com>
2026-08-20 15:48:08 +0200

    feat(notif): add notif types model

---
+++ packages/core/src/models/notif/notifType.ts
@@ -0,0 +1,7 @@
+export enum NotificationType {
+	NEW_CLAN_JOIN_REQUEST = 'newClanJoinRequest',
+	NEW_REWARD = 'newReward',
+	MARKET_OFFER_WIN = 'marketOfferWin',
+	MARKET_OFFER_SOLD = 'marketOfferSold',
+	MARKET_OFFER_UNSOLD = 'marketOfferUnsold'
+}
