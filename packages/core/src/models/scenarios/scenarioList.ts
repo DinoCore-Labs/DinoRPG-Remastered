b05f0693746ee8d0dd9dc522946acfee775716b1
Docalaco <louis.og31@gmail.com>
2026-08-20 15:31:52 +0200

    feat(dojo): add dojo scenario

--- packages/core/src/models/scenarios/scenarioList.ts
+++ packages/core/src/models/scenarios/scenarioList.ts
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 import { jeromeTraderScenario } from './data/jeromeTraderScenario.js';
 import { magnetiteScenario } from './data/magnetiteScenario.js';
+=======
+import { dojoScenario } from './data/dojoScenario.js';
+>>>>>>> 2cf879e2 (feat(dojo): add dojo scenario)
 import { merguezScenario } from './data/merguezScenario.js';
 import { pacScenario } from './data/pacScenario.js';
 import { starScenario } from './data/starScenario.js';
@@ -8,8 +12,12 @@ export const scenarioList = {
 	star: starScenario,
 	merguez: merguezScenario,
 	pac: pacScenario,
+<<<<<<< HEAD
 	magnet: magnetiteScenario,
 	jerome_trader: jeromeTraderScenario
+=======
+	dojo: dojoScenario
+>>>>>>> 2cf879e2 (feat(dojo): add dojo scenario)
 } as const;
 
 export type ScenarioKey = keyof typeof scenarioList;
