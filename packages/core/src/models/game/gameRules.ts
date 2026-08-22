1a35aa43752b2d38f709226c956b7af7a3585326
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-08-22 21:29:14 +0200

    refactor(game-rules): simplify game rules

--- packages/core/src/models/game/gameRules.ts
+++ packages/core/src/models/game/gameRules.ts
@@ -1,6 +1,6 @@
-export const GAME_RULES_VERSION = '1.0' as const;
+export const GAME_RULES_VERSION = '1.1' as const;
 
-export const GAME_RULES_UPDATED_AT = '2026-06-24' as const;
+export const GAME_RULES_UPDATED_AT = '2026-08-22' as const;
 
 export const GAME_RULES_ACCEPTANCE_REQUIRED_CODE = 'RULES_ACCEPTANCE_REQUIRED' as const;
 
