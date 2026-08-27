a87187e304b49622c6894c7c769c2bb9d5ccd24d
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-08-27 23:31:46 +0200

    fix(fight): add missing display / background

--- packages/core/src/models/place/placeListv2.ts
+++ packages/core/src/models/place/placeListv2.ts
@@ -1008,7 +1008,7 @@ export const placeListv2 = definePlaces({
 			{ target: PlaceEnum.VILLA, condition: 'scenario(nimbao,3+)' }
 		],
 		ground: GroundEnum.DIRT,
-		background: 'dw_plaza',
+		background: 's_dnv',
 		top: 110
 	},
 
