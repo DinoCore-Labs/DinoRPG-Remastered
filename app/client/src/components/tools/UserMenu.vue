c9aeeed88c6fee3fae90e505af45b64d65c867fb
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-08-27 22:42:02 +0200

    feat(tutorial): add contextual helpers

--- app/client/src/components/tools/UserMenu.vue
+++ app/client/src/components/tools/UserMenu.vue
@@ -29,7 +29,7 @@
 			</div>
 			<div class="shortCutMenu">
 				<div class="shortCuts" @click="closeOnShortcutLinkClick">
-					<RouterLink class="link" :to="`/user`">
+					<RouterLink class="link" :to="`/user`" data-tutorial="account">
 						<svg class="svgLinkIcon" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon">
 							<path
 								d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4m0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4"
