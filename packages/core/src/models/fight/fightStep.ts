8facdd111ad9c0547bf4a4ed94a27cd9aa48570a
Jérémy Lebourgeois <134381505+JeremyLebourgeois@users.noreply.github.com>
2026-09-02 00:28:08 +0200

    feat(fight): add visual aura effect when Beer blocks healing

--- packages/core/src/models/fight/fightStep.ts
+++ packages/core/src/models/fight/fightStep.ts
@@ -19,7 +19,7 @@ import { Skill } from '../skills/skillList.js';
 import { FightText } from './fightDialog.js';
 import { FighterType } from './fighterType.js';
 import { FightStatus } from './fightStatus.js';
-import { EntranceEffect, LifeEffect, NotificationList } from './transpiler.js';
+import { AuraFxType, EntranceEffect, LifeEffect, NotificationList } from './transpiler.js';
 
 export interface StepFighter {
 	id: number;
@@ -317,6 +317,13 @@ export interface FightTextStep {
 	text: FightText;
 }
 
+export interface AuraAntiHealStep {
+	action: 'aura';
+	fid: number;
+	type: AuraFxType;
+	color: string;
+}
+
 export type FightStep =
 	| TimeLimitStep
 	| TimeoutStep
@@ -360,4 +367,5 @@ export type FightStep =
 	| AnimationStep
 	| AttachStep
 	| LoseCostumeStep
-	| FightTextStep;
+	| FightTextStep
+	| AuraAntiHealStep;
