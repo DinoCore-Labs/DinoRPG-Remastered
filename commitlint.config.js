76ff42636fbd91273811c54bdea47c4e742efc5c
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-09-09 22:18:36 +0200

    version: 0.14.1

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,21 @@ export default {
 		'type-enum': [
 			2,
 			'always',
-			['build', 'chore', 'ci', 'content', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']
+			[
+				'build',
+				'chore',
+				'ci',
+				'content',
+				'docs',
+				'feat',
+				'fix',
+				'perf',
+				'refactor',
+				'revert',
+				'style',
+				'test',
+				'version'
+			]
 		]
 	}
 };
